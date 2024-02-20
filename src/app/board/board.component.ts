7072c390025f1fa71effcd6ee9b01e79eb70068c
Sharjeel-22 <pcraboot55441@gmail.com>
2024-02-20 20:29:56 +0500

    game

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+export class BoardComponent implements OnInit {
+
+  @Input() value: 'X' | 'O' | undefined;
+
+
+  constructor() { }
+  ngOnInit(): void {
+
+  }
+
+}
